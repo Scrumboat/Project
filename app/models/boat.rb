b8d07694e97bb3970f3c35f6df95ec732217b276
jtmikkon <jtmikkon@gmail.com>
2013-05-29 12:16:51 +0300

    -loggeri, +vestal_versions activerecord versioning

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,4 +1,6 @@
 class Boat < ActiveRecord::Base
+versioned
+attr_accessible :updated_by
 validates_presence_of :Omistaja
 validates_presence_of :RekPvm
 validates_presence_of :Nimi
@@ -11,5 +13,5 @@ validates_presence_of :Syvyys
 validates_presence_of :Vuosimalli
 
 has_many :members, :through => :ownerships
- attr_accessible :Huomautukset, :JnoOm, :JnoOs, :Katsastus, :Korkeus, :Laituri, :Leveys, :MuutosPvm, :Nimi, :Omistaja, :Pituus, :RekNro, :RekPvm, :Syvyys, :Tarra, :Teho, :Telakka, :Uppouma, :ValmMalli, :VenePuhA, :VenePuhB, :Vuosimalli, :tyyppi
+ attr_accessible :Huomautukset, :JnoOm, :JnoOs, :Katsastus, :Korkeus, :Laituri, :Leveys, :MuutosPvm, :Nimi, :Omistaja, :Pituus, :RekNro, :RekPvm, :Syvyys, :Tarra, :Teho, :Telakka, :Uppouma, :ValmMalli, :VenePuhA, :VenePuhB, :Vuosimalli, :tyyppi, :tag_attributes
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,6 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-require Rails.root.join('lib', 'assets') + 'log_utility.rb'
 
-$loggeri = Log_utility.new
 
 # Initialize the rails application
 Venerekisteri::Application.initialize!
