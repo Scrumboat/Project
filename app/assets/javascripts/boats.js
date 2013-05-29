1d54dcc876a27fdda64171d8db70551982bbf0ae
Joe Niemi <niemijoe@hotmail.com>
2013-05-29 23:44:52 +0300

    lomakkeen taydennys valmis

--- app/assets/javascripts/boats.js
+++ app/assets/javascripts/boats.js
@@ -1,14 +1,17 @@
-function testi() {
-
-	document.getElementById('boat_Leveys').value="100";
-
-}
-
 function taydenna() {
-       document.getElementById('boat_Pituus').value="50";
+  //     document.getElementById('boat_Pituus').value="50";
 	var e = document.getElementById('malli');
 	var nimi = e.options[e.selectedIndex].text;
 	document.getElementById('boat_ValmMalli').value=nimi;
+
+  if (!nimi || 0 === nimi.length) {
+   document.getElementById('boat_Pituus').value="";
+   document.getElementById('boat_Leveys').value="";
+   document.getElementById('boat_Syvyys').value="";
+   document.getElementById('boat_Uppouma').value="";
+   document.getElementById('boat_Korkeus').value="";
+   return;
+  }
 	$.getJSON('/mallis/'+nimi+'.json', function(data) {
           $.each(data, function(key, val) {
                   if (document.getElementById('boat_'+key) != null) {
@@ -19,5 +22,4 @@ function taydenna() {
           //var obj = JSON.parse(data);
           //document.getElementById('boat_Syvyys').value=obj.Syvyys;
 	});
-	
 }
