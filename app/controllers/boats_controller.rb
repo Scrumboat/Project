9506e3a53fc88bcabcfcbf8d7382f5551aa8a061
jtmikkon <jtmikkon@gmail.com>
2013-06-13 18:10:18 +0300

    boats_controller#update toimii nyt jäsennumeron pohjalta

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -37,7 +37,7 @@ class BoatsController < ApplicationController
 
   # GET /boats/1/edit
   def edit
-    @boat = Boat.find(params[:id])
+    @boat = Boat.find(params[:id], :include => :members)
   end
 
   # POST /boats
@@ -81,8 +81,9 @@ class BoatsController < ApplicationController
     @boat = Boat.find(params[:id], :include => [:members])
     #@boat = Boat.find(params[:id])
     #changeJnoToId
+    change_jno_to_id_for_update
     respond_to do |format|
-      if @boat.update_attributes(params[:boat] )
+      if @boat.update_attributes(params[:boat]) && @onkoOk
         format.html { redirect_to @boat, notice: 'Veneen muokkaus onnistui.' }
         format.json { head :no_content }
       else
@@ -102,6 +103,18 @@ class BoatsController < ApplicationController
       format.html { redirect_to boats_url }
       format.json { head :no_content }
     end
+  end
+
+  def change_jno_to_id_for_update
+    params[:boat][:BoatsMembers_attributes].values.each do |bm|
+      member = Member.find_by_Jno(bm[:member_id])
+      if member == nil
+        @onkoOk = false
+	return
+      end
+      bm[:member_id] = member.id
+    end if params[:boat] and params[:boat][:BoatsMembers_attributes]
+    @onkoOk = true
   end
 
   def changeJnoToId
