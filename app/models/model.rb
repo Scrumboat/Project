79a7a528d00ef37de663d9a76ab0c773adb7cee2
Nimeton <jukka.ukkonen@helsinki.fi>
2013-06-18 17:55:54 +0300

    Malli takas

--- app/models/model.rb
+++
@@ -1,3 +0,0 @@
-class Model < ActiveRecord::Base
-  attr_accessible :Korkeus, :Leveys, :Pituus, :Syvyys, :Uppouma, :ValmMalli, :tyyppi
-end
--- db/migrate/20130617190919_rename_mallis_to_models.rb
+++
@@ -1,9 +0,0 @@
-class RenameMallisToModels < ActiveRecord::Migration
-  def up
-	rename_table :mallis, :models
-  end
-
-  def down
-	rename_table :models, :mallis
-  end
-end
