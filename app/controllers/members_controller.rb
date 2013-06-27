834f722dd265e21befb1a50130570d0e5db23609
jtmikkon <jtmikkon@gmail.com>
2013-06-27 14:51:09 +0300

    oli postgre vaivaa  vieläkin sorttauksessa. toivottavasti ei enaa

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -101,7 +101,7 @@ class MembersController < ApplicationController
 
   private
   def sort_column
-    Member.column_names.include?(params[:sort]) ? params[:sort] : Member.connection.quote_column_name("Jno")
+    Member.column_names.include?(params[:sort]) ? Member.connection.quote_column_name(params[:sort]) : Member.connection.quote_column_name("Jno")
   end
   
   def sort_direction
