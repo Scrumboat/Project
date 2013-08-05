abc63a657d47dbb5cfa27bb7c58f4ab72132a5a4
jtmikkon <jtmikkon@gmail.com>
2013-08-05 17:32:35 +0300

    popover tulee hoverina

--- app/assets/javascripts/invoicefilter.js
+++ app/assets/javascripts/invoicefilter.js
@@ -16,3 +16,14 @@ function toggle(className, obj) {
     if ($input.prop('checked')) $(className).show();
     else $(className).hide();
 }
+
+$(':not(#anything)').on('click', function (e) {
+    $('.popover-link').each(function () {
+        //the 'is' for buttons that trigger popups
+        //the 'has' for icons and other elements within a button that triggers a popup
+        if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
+            $(this).popover('hide');
+            return;
+        }
+    });
+});
