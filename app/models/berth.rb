c4cebd6502319003ccfeef890dc2b7283743afb0
Joe Niemi <niemijoe@hotmail.com>
2013-08-19 15:03:05 +0300

    berth.number on uniikki

--- app/models/berth.rb
+++ app/models/berth.rb
@@ -2,11 +2,18 @@
 class Berth < ActiveRecord::Base
   belongs_to :dock
   belongs_to :boat
+
   attr_accessible :depth, :exists, :length, :number, :width, :dock_id, :reknro, :jno, :vartio, :boat_id
+
   before_save :reknro_default_value
+
+  validates_numericality_of :number, :only_integer => true
+  validates :jno, :presence => true, :uniqueness => true, length: {maximum: 2}, :numericality => { :greater_than_or_equal_to => 1 }
+
   def reknro_default_value
     self.reknro ||= ""
   end
+
   def printExists
   	print_exists = 'Ei'
   	if self[:exists]
