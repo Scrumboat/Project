90c301047f22f099aa38615963c42b7a5f4fa1fc
Joe <niemijoe@hotmail.com>
2013-08-29 13:05:58 +0300

    unohtu committaa laitoksel

--- app/models/guardseason.rb
+++ app/models/guardseason.rb
@@ -4,13 +4,13 @@ class Guardseason < ActiveRecord::Base
   attr_accessible :alku_pvm, :loppu_pvm, :nimi
 
   def self.luovartiovuorot(guardseason_id)
-    @members = Member.all.cycle
-    @member = @members.shift
+    #@members = Member.all.cycle
+    #@member = @members.shift
     @guardseason = Guardseason.where(:id => guardseason_id).first
     @paivat = (@guardseason.alku_pvm..@guardseason.loppu_pvm)
     @paivat.each do |paiva|
-        Guardturn.luovartiovuorot(guardseason_id, paiva, @member)
-        @member = @members.shift
+        Guardturn.luovartiovuorot(guardseason_id, paiva, 2222)
+        #@member = @members.shift
     end
   end
 end
